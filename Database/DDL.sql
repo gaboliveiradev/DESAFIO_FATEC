93bd87c3e3038d9dfd613cdafc506e0d953bee1a
gaboliveiradev <g38789138@gmail.com>
2022-07-29 20:26:07 -0300

    construindo o ddl do banco de dados

---
+++ Database/DDL.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE agatecnologia_db;
+USE agatecnologia_db;
+
+CREATE TABLE pergunta (
+    id int auto_increment NOT NULL,
+    titulo text NOT NULL,
+    primary key (id)
+);
+
+CREATE TABLE resposta (
+    id int auto_increment NOT NULL,
+    id_pergunta int NOT NULL,
+    descricao text NOT NULL,
+    primary key (id),
+    foreign key (id_pergunta) references pergunta (id)
+);
+
+CREATE TABLE funcionario (
+    id int auto_increment NOT NULL,
+    nome varchar(150) NOT NULL,
+    email varchar(150) NOT NULL,
+    senha varchar(100) NOT NULL,
+    adm char(1) NOT NULL DEFAULT "N",
+    primary key (id)
+);
