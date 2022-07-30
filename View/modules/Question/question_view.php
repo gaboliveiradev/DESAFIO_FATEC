94b622dd92ee1c3f585d0f949b61e14cdbae6e7e
gaboliveiradev <g38789138@gmail.com>
2022-07-30 13:44:37 -0300

    reestilização da página de perguntas criadas

--- View/modules/Question/question_view.php
+++ View/modules/Question/question_view.php
@@ -12,31 +12,43 @@
     <header>
         <?php include "./View/includes/cabecalho.php" ?>
     </header>
-    <main class="container-question flex">
-        <section class="section-question">
-            <h5>Perguntas criadas</h5>
-            <hr>
-            <?php foreach ($model->arr_question as $item): ?>
-                <div>
-                    <p>
-                        <?= $item->id ?> - <?= $item->descricao ?>
-                        <div style="text-align: right;">
-                            <a style="text-align: left;" href="/question/form?id=<?= $item->id ?>">
-                                Editar
-                            </a>
-                            <a href="/question/delete?id=<?= $item->id ?>">
-                                <i class='bx bxs-trash'></i>
-                            </a>
+
+    <main>
+        <section>
+
+        </section>
+        <section>
+            <div class="container-search">
+                <form class="form-search d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Buscar por palavra-chave" aria-label="Search">
+                    <button class="btn btn-primary" type="submit">Buscar</button>
+                </form>
+            </div>
+            <div class="header-questions">
+                <h4>Perguntas Criadas</h4>
+            </div>
+            <div class="container-questions">
+                <?php foreach ($model->arr_question as $item): ?>
+                    <div class="card">
+                        <div class="card-body">
+                            <?= $item->descricao ?>  
+                            <div style="text-align: right;">
+                                <a style="text-align: left;" href="/question/form?id=<?= $item->id ?>">
+                                    Editar
+                                </a>
+                                <a href="/question/delete?id=<?= $item->id ?>">
+                                    <i class='bx bxs-trash'></i>
+                                </a>
+                            </div>
                         </div>
+                    </div>
+                <?php endforeach ?>
+                <?php if (count($model->arr_question) == 0) : ?>
+                    <p>
+                        <a href="/question/form">Não existe nenhuma pergunta registrada em nosso questionário, clique aqui para cadastrar sua primeira pergunta.</a>
                     </p>
-                </div>
-                <hr>
-            <?php endforeach ?>
-            <?php if (count($model->arr_question) == 0) : ?>
-                <p>
-                    <a href="/question/form">Não existe nenhuma pergunta registrada em nosso questionário, clique aqui para cadastrar sua primeira pergunta.</a>
-                </p>
-            <?php endif ?>
+                <?php endif ?>
+            </div>
         </section>
     </main>
     <?php include "./View/includes/js_config.php" ?>
