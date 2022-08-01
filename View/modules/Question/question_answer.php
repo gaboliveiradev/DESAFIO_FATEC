e06c4ee201a16d6da4c7197129e0b86b012929a5
gaboliveiradev <g38789138@gmail.com>
2022-08-01 14:57:59 -0300

    correções de bugs

--- View/modules/Question/question_answer.php
+++ View/modules/Question/question_answer.php
@@ -17,37 +17,41 @@
         <section class="section-header">
             <h1>Questionário</h1>
         </section>
-        <section class="section-questions">
-          <form action="/answer/save" method="POST">
-            <?php 
-              $pos = 1;
-            ?>
-            <?php foreach($arr_question as $item): ?>
-              <hr>
-              <div class="card">
-                <div class="card-header">
-                  <strong>Pergunta <?= $pos ?></strong>
-                </div>
-                <div class="card-body">
-                  <p class="card-text">
-                      <?= $item->descricao ?>
-                  </p>
-                  <div class="mb-3">
-                    <input type="hidden" name="id_<?= $pos ?>" value="<?= $item->id ?>">
-                    <textarea name="question_<?= $pos ?>" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        <?php if(!count($arr_question) == 0): ?>
+          <section class="section-questions">
+            <form action="/answer/save" method="POST">
+              <?php 
+                $pos = 1;
+              ?>
+              <?php foreach($arr_question as $item): ?>
+                <hr>
+                <div class="card">
+                  <div class="card-header">
+                    <strong>Pergunta <?= $pos ?></strong>
+                  </div>
+                  <div class="card-body">
+                    <p class="card-text">
+                        <?= $item->descricao ?>
+                    </p>
+                    <div class="mb-3">
+                      <input type="hidden" name="id_<?= $pos ?>" value="<?= $item->id ?>">
+                      <textarea name="question_<?= $pos ?>" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
                   </div>
                 </div>
+                <?php 
+                  $pos++;
+                ?>
+              <?php endforeach; ?>
+              <br>
+              <div class="d-grid gap-2">
+                <button class="btn btn-primary" type="submit">Enviar</button>
               </div>
-              <?php 
-                $pos++;
-              ?>
-            <?php endforeach; ?>
-            <br>
-            <div class="d-grid gap-2">
-              <button class="btn btn-primary" type="submit">Enviar</button>
-            </div>
-          </form>
-        </section>
+            </form>
+          </section>
+        <?php else: ?>
+          <p>Não existe nenhuma pergunta neste questionário.</p>
+        <?php endif; ?>
     </main>
     <?php include "./View/includes/js_config.php" ?>
 </body>
